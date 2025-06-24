fb5d336d590207847551d7b017d880971eb34f43
Sid-op777 <69779993+Sid-op777@users.noreply.github.com>
2025-06-24 19:36:13 +0530

    fixed build

--- src/test/java/com/microproject/linktoolkit/LinkToolkitApplicationTests.java
+++ src/test/java/com/microproject/linktoolkit/LinkToolkitApplicationTests.java
@@ -1,10 +1,23 @@
 package com.microproject.linktoolkit;
 
+import com.azure.storage.blob.BlobServiceClient;
+import com.maxmind.geoip2.DatabaseReader;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ua_parser.Parser;
 
 @SpringBootTest
 class LinkToolkitApplicationTests {
+
+    @MockBean
+    private BlobServiceClient blobServiceClient;
+
+    @MockBean
+    private DatabaseReader geoIpDatabaseReader;
+
+    @MockBean
+    private Parser userAgentParser;
 
     @Test
     void contextLoads() {
